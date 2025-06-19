e27faff0506cbf653a9ef227c1d8c3c7d4e5c013
Darwin_Salcie <d.salcie1@unapec.edu.do>
2025-06-19 17:19:58 -0400

    Swagger only in development Disabled

--- API/Program.cs
+++ API/Program.cs
@@ -59,12 +59,12 @@ c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 //Habilitar CORS
